9a0c8ecdb314a43c84371b66c2eaf0f2073c662b
Nahël <nahel@example.com>
2026-06-07 22:25:02 +0200

    feat: initialize Firebase Firestore configuration and secure environment variables in gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQBOLk7AXSzYH8_St5cD3nEMTiFrRHLiw",
-  authDomain: "site-esteban.firebaseapp.com",
-  projectId: "site-esteban",
-  storageBucket: "site-esteban.firebasestorage.app",
-  messagingSenderId: "681918267715",
-  appId: "1:681918267715:web:fea52ae4f28f8231ef2ff6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
